bb926ace6ee8f3e286969505cee06ee1d9d3692e
Danilo Silva <danilo.silva@msn.com>
2023-01-31 13:04:15 -0300

    Criando a Entidade Base

---
+++ ListaTarefas/ListaTarefas.Domain/Entities/EntityBase.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ListaTarefas.Domain.Entities
+{
+    public abstract class EntityBase : IEquatable<EntityBase>
+    {
+        public EntityBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        public bool Equals(EntityBase other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
